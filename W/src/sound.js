a1541e5f9b9a56c6dfdb31d90a63545d92d20ad1
m0x0m0x <mx0mx@tuta.io>
2024-05-01 00:04:46 +0530

    addSexNosie1sec

--- W/src/sound.js
+++ W/src/sound.js
@@ -1,8 +1,17 @@
 /// Sound on hover of logo
 const logoHover = document.querySelector(".logo");
 const logoHoverSound = document.getElementById("logoHoverSound");
+
 logoHover.addEventListener("mouseover", () => {
-  logoHoverSound.currentTime = 0; // Reset the audio to the beginning
-  logoHoverSound.play(); // Play the click sound
+  // Set the start time of the audio
+  logoHoverSound.currentTime = 0; // or any other start time in seconds
+
+  // Play the audio
+  logoHoverSound.play();
+
+  // Stop the audio after a certain amount of time has elapsed
+  setTimeout(() => {
+    logoHoverSound.pause();
+    logoHoverSound.currentTime = 0;
+  }, 400); // or any other duration in milliseconds
 });
-//////////////////

9f4e64eda62158893d3f875de159966930130659
m0x0m0x <mx0mx@tuta.io>
2024-04-30 23:19:45 +0530

    fix

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
 console.log("Hello!");
-
-////////////////////
-// Javascript to change body color on hover
-const logo = document.querySelector(".logo");
-const body = document.querySelector("body");
-
-logo.addEventListener("mouseover", () => {
-  body.classList.add("logo-hover");
-});
-
-logo.addEventListener("mouseout", () => {
-  body.classList.remove("logo-hover");
-});

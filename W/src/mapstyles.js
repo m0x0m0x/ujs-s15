403faa854d7ac456b1ef242e4ffc58cb6877ee4e
m0x0m0x <mx0mx@tuta.io>
2024-05-01 21:32:36 +0530

    fix jmap styles

---
+++ W/src/mapstyles.js
@@ -0,0 +1,33 @@
+/* 
+These are mapstyles that can be used with the map will dump the actual code here and then call it in index.html 
+Maps are taken from .
+
+https://leaflet-extras.github.io/leaflet-providers/preview/
+- Bunch of styles here
+*/
+
+/////////////////////////////////////////////////////////
+// Stadia statellite style
+export const Stadia_AlidadeSatellite = L.tileLayer(
+  "https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.{ext}",
+  {
+    minZoom: 0,
+    maxZoom: 20,
+    attribution:
+      '&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    ext: "jpg",
+  }
+);
+// Add this in the index.html to activate
+// Stadia_AlidadeSatellite.addTo(map);
+/////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////
+// Default styles
+export const DefaultStyle = L.tileLayer(
+  "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+  {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }
+);
+///////////////////////////////////////////////////////////

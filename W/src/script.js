d6cf09eb82b1d5ab7ace45bb4c795c1c3874c65c
m0x0m0x <mx0mx@tuta.io>
2024-05-01 18:06:21 +0530

    addMap

--- W/src/script.js
+++ W/src/script.js
@@ -38,6 +38,19 @@ if (navigator.geolocation)
         `
       );
 
+      // Leaflet js code
+      const map = L.map("map").setView([51.505, -0.09], 13);
+
+      L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker([51.5, -0.09])
+        .addTo(map)
+        .bindPopup("A pretty CSS popup.<br> Easily customizable.")
+        .openPopup();
+
       // Getting latitude and longitude of interesting places
     },
     function () {

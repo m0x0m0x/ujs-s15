d0e352baae5e1d38c76d4c33f543cf3127b6f13a
m0x0m0x <mx0mx@tuta.io>
2024-05-01 17:13:38 +0530

    add location

--- W/src/script.js
+++ W/src/script.js
@@ -1,1 +1,35 @@
 "use strict";
+
+//////////////// Given Code ////////////////////////
+
+//prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector(".form");
+const containerWorkouts = document.querySelector(".workouts");
+const inputType = document.querySelector(".form__input--type");
+const inputDistance = document.querySelector(".form__input--distance");
+const inputDuration = document.querySelector(".form__input--duration");
+const inputCadence = document.querySelector(".form__input--cadence");
+const inputElevation = document.querySelector(".form__input--elevation");
+
+//////////////// Your Work ////////////////////////
+
+// Accessing the geolocation
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      //  Testing if geolocation api works
+      //   console.log("Your location faggot");
+      //   console.log(position);
+
+      // Storing by destructuring
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(latitude, longitude);
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+    },
+    function () {
+      alert("Fucker No Location - Bastard");
+    }
+  );

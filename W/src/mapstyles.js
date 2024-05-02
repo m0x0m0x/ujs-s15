faef96c7d65808c022f0d7ada86332eacee90433
m0x0m0x <mx0mx@tuta.io>
2024-05-02 23:25:15 +0530

    ccs

--- W/src/mapstyles.js
+++ W/src/mapstyles.js
@@ -13,8 +13,7 @@ export const Stadia_AlidadeSatellite = L.tileLayer(
   {
     minZoom: 0,
     maxZoom: 20,
-    attribution:
-      '&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    attribution: "👙",
     ext: "jpg",
   }
 );
--- W/src/script.js
+++ W/src/script.js
@@ -20,7 +20,10 @@ const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
 
-//////////////// Your Work ////////////////////////
+//////////////// Your Work //////////////////////////
+
+//Global variable
+let map, mapEvent;
 
 // Accessing the geolocation
 if (navigator.geolocation)
@@ -48,7 +51,7 @@ if (navigator.geolocation)
       //////////////////////////////////
       const AUScoords = [-30.750077700995366, 121.46753346785351];
       const bbwSC = [34.09963655644939, -118.28935780082956];
-      const map = L.map("map").setView(bbwSC, 15);
+      map = L.map("map").setView(bbwSC, 15);
 
       // Examining the map object
       console.log(map);
@@ -58,35 +61,43 @@ if (navigator.geolocation)
       // DefaultStyle.addTo(map);
       // Stadia_AlidadeSmoothDark.addTo(map);
 
-      // Geting the coordinates when clicking on the map
-      map.on("click", function (mapEvent) {
+      // Geting the coordinates when clicking on the map - Handling click on maps
+      map.on("click", function (mapE) {
+        mapEvent = mapE;
         // Observe the map event
         console.log(mapEvent);
 
         // Setting up the form
         form.classList.remove("hidden");
         inputDistance.focus();
-
-        // const { lat, lng } = mapEvent.latlng;
-        // Add marker
-        // L.marker([lat, lng]).addTo(map).bindPopup("Mistresss").openPopup();
-        // // Creating a popup object
-        // L.marker([lat, lng])
-        //   .addTo(map)
-        //   .bindPopup(
-        //     L.popup({
-        //       maxWidth: 250,
-        //       minWidth: 100,
-        //       autoClose: false,
-        //       closeOnClick: false,
-        //       className: "running-popup",
-        //     })
-        //   )
-        //   .setPopupContent("Booty")
-        //   .openPopup();
       });
     },
     function () {
       alert("Fucker No Location - Bastard");
     }
   );
+
+// Form interactivity here
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  // Display Marker
+  const { lat, lng } = mapEvent.latlng;
+
+  // Add marker
+  L.marker([lat, lng]).addTo(map).bindPopup("Mistresss").openPopup();
+
+  // Creating a popup object
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: "running-popup",
+      })
+    )
+    .setPopupContent("Booty")
+    .openPopup();
+});
